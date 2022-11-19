563541f1206d1a07c9b9f0e6710e4e9e9fa88908
Noxlumina <60930568+Noxlumina@users.noreply.github.com>
2022-11-19 14:10:15 +0100

    Update rollercoaster.js

--- rollercoaster.js
+++ rollercoaster.js
@@ -1,37 +1,31 @@
-let files = [2, 3, 5, 4];
+const read = require("readline-sync");
+console.log("---Roller-Coaster---")
 
-let participant = 0;
+const nb_tours = read.questionInt("Nombre de tours ? ");
+let nb_file = [2, 3, 5, 4];
+console.log("file devant le manege:")
+console.log(nb_file);
+// variable recherchée
+let profit = 0;
+// variable du reste de place dans le wagon
+//let nb_place_rest = 5;
 
-const tours = 5;
-const places = 5;
-
-for (i = 0; (i = tours); i++) {
-  if (files[0] > 5) {
-    console.log("le manège n'a que 5 places");
-  } else if (files[0] < 5 && files[0] + files[1] > 5) {
-    participant = files[0];
-  } else if (files[0] < 5 && files[0] + files[1] + files[2] > 5) {
-    participant = files[1];
-  }
-
-  else if (files[0] < 5 && files[0] + files[1] + files[2]+ files[3] > 5) {
-    participant = files[2];
-  }
-
-  else if (files[0] < 5 && files[0] + files[1] + files[2]+ files[3]+ files[4] > 5) {
-    participant = files[3];
-  }
-
-  else if (files[0] < 5 && files[0] + files[1] + files[2]+ files[3]+ files[4]+ files[5] > 5) {
-    participant = files[4];
-  }
-
-  else if (files[0] < 5 && files[0] + files[1] + files[2]+ files[3]+ files[4]+ files[5] == 5) {
-    participant = files[5];
-  }
-
-
-  for (j = 0; (j = files.length); j++) {
-    let file = files[index];
+// la boucle for sert à compter le nombre de tours restants
+for (let i = 1; i <= nb_tours; i++) {
+  nb_place_rest = 5;
+  // cette boucle for permet de savoir le nombre de place restant dans le wagon
+  for (let y = 0; y < nb_file.length; y++) {
+    nb_place_rest = nb_place_rest - nb_file[0];
+    // si le wagon n'est pas plein
+    if (nb_place_rest >= 0) {
+      val = nb_file[0];
+      // on rajoute du profit ( c'est les personnes qui rentre dans l'attraction)
+      profit += val * 1;
+      // ici on modifie la fille d'attente
+      nb_file.shift();
+      nb_file.push(val);
+    }
   }
 }
+// à la fin de la journéee, on affiche le profit
+console.log(`Profits en fin de journée -> ${profit}€`);
