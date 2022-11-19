0e252aa34c9fa5150fee2fc18722074516072164
Noxlumina <60930568+Noxlumina@users.noreply.github.com>
2022-11-19 13:41:55 +0100

    Update exo12.js

--- exo12.js
+++ exo12.js
@@ -1,26 +1,6 @@
-const read = require("readline-sync");
-
-/*let temperatures = [12,25,5,7,6,-5];
-let diff = temperatures[0]
-
-for (let i = 0; i < temperatures.length; i++)
-{
-    let temperature = temperatures[i];
-    let diff1= Math.abs(0- temperature); 
-    if (diff1<diff )
-    {
-        diff = diff1;
-        let tempro = temperature;
-    }
-    else if ((diff1 = diff) && Math.sign(temperature<0))
-    {
-        tempro = temperature;
-    }
-
-}
-console.log(`La température la plus proche est ${tempro}`);
-*/
+console.log("---Liste des températures---");
 let temperatures = [12, 25, 5, 7, 6, -5,-3,3];
+console.log(temperatures);
 let tempProche = temperatures[0];
 
 for (let index = 0; index < temperatures.length; index++) {
@@ -34,4 +14,5 @@ for (let index = 0; index < temperatures.length; index++) {
     tempProche = temperature;
   }
 }
-console.log(tempProche);
+console.log(`La tempétature la plus basse est ${tempProche} °C`);
+
