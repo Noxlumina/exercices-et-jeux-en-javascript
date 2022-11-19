0a7c9d90e6e47165a6ca38b8342e8950b2a9970f
Noxlumina <60930568+Noxlumina@users.noreply.github.com>
2022-11-19 14:15:38 +0100

    Update exo10.js

--- exo10.js
+++ exo10.js
@@ -1,21 +1,19 @@
-const read = require("readline-sync");
-
-console.log("pyramid");
-
-let nb = read.questionFloat("Nombre ? ");
-var stars = "";
-
-for (var i=1; i<=nb;i++)
-{
-//console.log(i);
-stars=stars+" ";
-}
-stars=stars+"*";
-console.log(stars);
-
-for (var i=nb; i>1;i--)
-{
-    stars= stars + "**";
-    sarts=stars.substring(2);
-    console.log(stars); 
+const read = require('readline-sync');
+console.log("---true pyramide---")
+let val = +read.question("Nombre d'etage de la pyramide:");
+let etoile = '';
+let espace = "";
+let affichage = "";
+for(let i= 1; i <= val; i++){
+    if(i === 1){
+        etoile = "*";
+        espace = "    ";
+        affichage = espace + etoile;
+        console.log(`${affichage}`);
+    }else{
+        etoile += "**";
+        espace = espace.substring(0, espace.length - 1);
+        affichage = espace + etoile;
+        console.log(`${affichage}`);
+    }
 }
