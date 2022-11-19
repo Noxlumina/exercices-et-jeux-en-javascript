02008e44c2d9dd9ef411bcdcdcb674af0e8c1842
Noxlumina <60930568+Noxlumina@users.noreply.github.com>
2022-11-19 13:15:47 +0100

    Update exo2.js

--- exo2.js
+++ exo2.js
@@ -1,4 +1,7 @@
-let val1 = 5;
-let val2 = 12;
-/* let result = val1 + val2 */
+const read = require("readline-sync");
+
+console.log("---Addition de deux nombres---")
+let val1 = read.questionInt("Quel est votre premier nombre ? ");
+let val2 = read.questionInt("Quel est votre deuxième nombre ? ");
+
 console.log(`${val1} + ${val2} = ${val1+val2}`);
